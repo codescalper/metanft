15116cb1595030f915f5cbfec157ed75da1c3437
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2024-03-06 10:24:03 +0000

    Update styling for card in MetaNFT page

--- app/metanft/page.tsx
+++ app/metanft/page.tsx
@@ -49,15 +49,15 @@ export default function Home(){
                 <Card className="xl:w-1/2 mx-auto relative w-3/4">
                 <div className="bg-card rounded-lg shadow-md p-6">
                     <img src={data.imageUrl} alt={data.name} className="w-full h-auto rounded-lg mb-4" />
-                    <h2 className="text-2xl font-bold text-muted-foreground underline underline-offset-4 decoration-orange-800 mb-2">{data.name}</h2>
+                    <h2 className="text-2xl font-bold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800 mb-2">{data.name}</h2>
                     <p className="text-gray-200 mb-4">{data.description}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800 ">Symbol:</span> {data.symbol}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800">Total Supply:</span> {data.totalSupply}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800">Token Type:</span> {data.tokenType}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800">Contract Address:</span> {data.address}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800">Contract Deployer:</span> {data.contractDeployer}</p>
-                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold text-muted-foreground underline underline-offset-4 decoration-orange-800">Twitter Username:</span> <Badge variant="outline"><BsTwitterX className="mr-[0.5rem]" /> {data.twitterUsername} </Badge></p>
-                    <p className=" text-wrap mb-2 font-semibold underline underline-offset-4 text-muted-foreground decoration-orange-800">Discord Server:{"  "}{"  "}<a href={data.discordUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline"><Badge variant="outline"> {"  "}<FaDiscord className="mr-[0.5rem]" />  {serverName} </Badge></a></p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800 ">Symbol:</span> {data.symbol}</p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800">Total Supply:</span> {data.totalSupply}</p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800">Token Type:</span> {data.tokenType}</p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800">Contract Address:</span> {data.address}</p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800">Contract Deployer:</span> {data.contractDeployer}</p>
+                    <p className="text-gray-200 text-wrap mb-2"><span className="font-semibold bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent underline underline-offset-4 decoration-orange-800">Twitter Username:</span> <Badge variant="outline"><BsTwitterX className="mr-[0.5rem]" /> {data.twitterUsername} </Badge></p>
+                    <p className=" text-wrap mb-2 font-semibold underline underline-offset-4 bg-gradient-to-l from-pink-400 via-red-500 to-orange-400 bg-clip-text text-transparent decoration-orange-800">Discord Server:{"  "}{"  "}<a href={data.discordUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline"><Badge variant="outline"> {"  "}<FaDiscord className="mr-[0.5rem]" />  {serverName} </Badge></a></p>
                 </div>
             </Card>
 
